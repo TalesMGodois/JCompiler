96c9272dd7ceaef22db158311b005360855b0bf4
TalesMGodois <tales.godois@gmail.com>
2015-11-15 21:49:59 -0200

    Mlehorando a interface e classe Lexico

--- src/analyser/lexico/Lexico.java
+++ src/analyser/lexico/Lexico.java
@@ -1,13 +1,63 @@
 package analyser.lexico;
 
 import signature.ILexico;
+import signature.IToken;
+
+import java.util.Hashtable;
 
 /**
  * Created by tales on 15/11/15.
  */
 public class Lexico implements ILexico{
-    @Override
-    public String getToken() {
-        return null;
+    private Hashtable<String,Token> SymbolTable;
+
+    public Lexico(){
+        this.SymbolTable = new Hashtable<String, Token>();
+        this.loadReservedKeyWords();
     }
+
+
+    public void loadReservedKeyWords() {
+        this.SymbolTable.put("inicio",new Token("inicio","inicio","palavraREservada"));
+        this.SymbolTable.put("varinicio",new Token("varinicio","varinicio","palavraREservada"));
+        this.SymbolTable.put("varfim",new Token("varfim","varfim","palavraREservada"));
+        this.SymbolTable.put("escreva",new Token("escreva","escreva","palavraREservada"));
+        this.SymbolTable.put("leia",new Token("leia","leia","palavraREservada"));
+        this.SymbolTable.put("se",new Token("se","se","palavraREservada"));
+        this.SymbolTable.put("entao",new Token("entao","entao","palavraREservada"));
+        this.SymbolTable.put("fimse",new Token("fimse","fimse","palavraREservada"));
+        this.SymbolTable.put("fim",new Token("fim","inicio","palavraREservada"));
+    }
+
+    @Override
+    public Token getToken(char key) {
+        return this.SymbolTable.get(key);
+    }
+
+    @Override
+    public void addToken(String key,Token tk){
+        this.SymbolTable.put(key,tk);
+    }
+
+    @Override
+    public Token[] getTokensFromLine(String line) {
+        Token[] tokens = new Token[line.length()];
+        for(int i=0;i<line.length();i++){
+            tokens[i] = this.getToken(line.charAt(i));
+            System.out.println(line.charAt(i));
+        }
+
+        return tokens ;
+    }
+
+    @Override
+    public boolean isToken(String key) {
+        Token token = this.SymbolTable.get(key);
+        if(token != null){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 }
--- src/signature/ILexico.java
+++ src/signature/ILexico.java
@@ -1,12 +1,20 @@
 package signature;
 
+import analyser.lexico.Token;
+
 /**
  * Created by tales on 15/11/15.
  */
 public interface ILexico {
 
-    public String getToken();
+    public Token getToken(char key);
 
+    public void loadReservedKeyWords();
 
+    public void addToken(String key,Token tk);
+
+    public Token[] getTokensFromLine(String line);
+
+    public boolean isToken(String key);
 
 }
